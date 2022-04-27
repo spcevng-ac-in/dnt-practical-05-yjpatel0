ce99a337f2e8788c0795220a8c3cb980d3d14723
yjpatel0 <61009655+yjpatel0@users.noreply.github.com>
2022-04-27 11:45:36 +0530

    Add files via upload

---
+++ dnt-practical-05main/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Practical_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter The Anything:");
+            string text = Console.ReadLine();
+            File.WriteAllText("file.txt", text);
+            try
+            {
+                string entereddata = File.ReadAllText("file.txt");
+                Console.WriteLine(entereddata);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
